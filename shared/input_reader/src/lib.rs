7834b6abddcc45072ab864afff8df04cb0673cfd
Spencer Dent <mr.spencerdent@gmail.com>
2022-08-12 11:32:51 -0400

    feat: create `read_lines_as_vec_i32` fn
    This will most likely be used on multiple days of the calendar.
    It is a bit specific, and could be written to be more general,
    but, as it stands now, it does the job, and does it efficiently.

--- shared/input_reader/src/lib.rs
+++ shared/input_reader/src/lib.rs
@@ -1,7 +1,26 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+#![deny(clippy::pedantic)]
+#![deny(clippy::unwrap_used)]
+#![deny(clippy::expect_used)]
+
+use std::{fs, io};
+
+/// Reads the file at the given path and returns its contents, line-by-line as a vector of 32-bit
+/// signed integers.
+///
+/// # Errors
+///
+/// If something goes wrong while trying to read the file -- file does not exist, user does not
+/// have read access to the file, etc -- this function will exit early, returning the resulting
+/// file `Error`.
+pub fn read_lines_as_vec_i32(path: &str) -> io::Result<Vec<i32>> {
+    let contents = fs::read_to_string(path)?;
+
+    let values = contents
+        .trim()
+        .split('\n')
+        .map(String::from)
+        .filter_map(|data| data.parse::<i32>().ok())
+        .collect();
+
+    Ok(values)
 }
