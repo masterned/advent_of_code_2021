b94c1f6b3e0800f865179f82b792a8ced966e0c9
Spencer Dent <mr.spencerdent@gmail.com>
2022-08-22 09:47:28 -0400

    refactor: extract functions for easier reuse
    - remove `calculate_rates` god-function
    - extract `calculate_bit_weights`
    - extract `convert_bit_weight_to_binary_string`
    - extract `invert_binary_string`

--- 3_binary_diagnostic/src/lib.rs
+++ 3_binary_diagnostic/src/lib.rs
@@ -1,22 +1,4 @@
-pub fn calculate_rates(bit_weights: &[i32]) -> (i32, i32) {
-    let (gamma_string, epsilon_string): (String, String) = bit_weights
-        .iter()
-        .map(|weight| {
-            if weight.is_positive() {
-                ('1', '0')
-            } else {
-                ('0', '1')
-            }
-        })
-        .unzip();
-
-    let gamma = i32::from_str_radix(&gamma_string, 2).expect("Not a binary string");
-    let epsilon = i32::from_str_radix(&epsilon_string, 2).expect("Not a binary string");
-
-    (gamma, epsilon)
-}
-
-pub fn calculate_power_consumption(diagnostic_report: &[String]) -> i32 {
+fn calculate_bit_weights(diagnostic_report: &[String]) -> Vec<i32> {
     let mut bit_weights: Vec<i32> = vec![0; diagnostic_report[0].len()];
 
     for byte in diagnostic_report {
@@ -29,11 +11,37 @@ pub fn calculate_power_consumption(diagnostic_report: &[String]) -> i32 {
                     bit_weights[index] += 1;
                 }
                 _ => {}
-            }
+            };
         }
     }
 
-    let (gamma_rate, epsilon_rate) = calculate_rates(&bit_weights);
+    bit_weights
+}
+
+fn convert_bit_weight_to_binary_string(bit_weights: &[i32]) -> String {
+    bit_weights
+        .iter()
+        .map(|&weight| if weight.is_positive() { '1' } else { '0' })
+        .collect()
+}
+
+fn invert_binary_string(binary_string: &str) -> String {
+    binary_string
+        .chars()
+        .map(|bit| match bit {
+            '0' => '1',
+            '1' => '0',
+            _ => bit,
+        })
+        .collect()
+}
+
+pub fn calculate_power_consumption(diagnostic_report: &[String]) -> i32 {
+    let bit_weights = calculate_bit_weights(diagnostic_report);
+    let gamma_string = convert_bit_weight_to_binary_string(&bit_weights);
+    let epsilon_string = invert_binary_string(&gamma_string);
+    let gamma_rate = i32::from_str_radix(&gamma_string, 2).expect("Not a binary string");
+    let epsilon_rate = i32::from_str_radix(&epsilon_string, 2).expect("Not a binary string");
 
     gamma_rate * epsilon_rate
 }
